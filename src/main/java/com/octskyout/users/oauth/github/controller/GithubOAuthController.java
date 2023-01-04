66f36b610d4493fff8e31dc1fbd1a9bacccfcacc
oct-sky-out <kms3335k@naver.com>
2023-01-04 16:31:35 +0900

    github oauth callback 요청 컨트롤러 테스트

--- src/main/java/com/octskyout/users/oauth/github/controller/GithubOAuthController.java
+++ src/main/java/com/octskyout/users/oauth/github/controller/GithubOAuthController.java
@@ -4,12 +4,14 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import com.octskyout.users.oauth.github.adapter.GithubOauthAdapter;
+import com.octskyout.users.oauth.github.dto.GithubUserDto;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.Link;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,5 +33,15 @@ public class GithubOAuthController {
         );
     }
 
+    @GetMapping("/callback")
+    public EntityModel<GithubUserDto> successLogin(@RequestParam String code,
+                                                   @RequestParam String state) {
+        GithubUserDto user = githubOauthAdapter.processOAuthLogin(code, state);
 
+        var selfRel = methodOn(GithubOAuthController.class).successLogin(code, state);
+
+        return EntityModel.of(user,
+            linkTo(selfRel).withSelfRel(),
+            Link.of(user.htmlUrl()).withRel("githubHtml"));
+    }
 }
--- src/test/java/com/octskyout/users/oauth/github/controller/GithubOAuthControllerTest.java
+++ src/test/java/com/octskyout/users/oauth/github/controller/GithubOAuthControllerTest.java
@@ -1,6 +1,7 @@
 package com.octskyout.users.oauth.github.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
@@ -11,6 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.octskyout.users.oauth.github.adapter.GithubOauthAdapter;
+import com.octskyout.users.oauth.github.dto.GithubUserDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -27,12 +29,12 @@ class GithubOAuthControllerTest {
     private GithubOauthAdapter githubOauthAdapter;
 
     @Test
-    void trySignInAtGithubOauth() throws Exception {
+    void 사용자의_깃허브_로그인_접속시도시_요청을_받는다() throws Exception {
         String adapterResult = "https://github.com/login?client_id=8dd94b69615b6ca2abe5&return_to=%2Flogin%2Foauth%2Fauthorize%3Fclient_id%3D8dd94b69615b6ca2abe5%26redirect_uri%3Dhttp%253A%252F%252F127.0.0.1%253A8081%252Fapi%252Flogin%252Foauth2%252Fgithub%26scope%3Duser%26state%3Dd2170c64-4c90-47fd-9f6c-d028e6af1756";
         given(githubOauthAdapter.requestGithubOauthSignIn())
             .willReturn(adapterResult);
 
-        mockMvc.perform(get("/api/login/oauth2/github"))
+        mockMvc.perform(get("http://github.com/example"))
             .andExpect(status().isOk())
             .andExpect(header().string("Content-Type", MediaTypes.HAL_JSON_VALUE))
             .andExpect(jsonPath("$.result").value("OK"))
@@ -43,5 +45,32 @@ class GithubOAuthControllerTest {
         then(githubOauthAdapter)
             .should(times(1))
             .requestGithubOauthSignIn();
+    }
+
+    @Test
+    void 깃허브로부터오는_콜백요청을_받는다() throws Exception {
+        String username = "example";
+        String id = "id";
+        String avatar = "http://github.com/example/avartar";
+        String html = "http://github.com/example";
+        given(githubOauthAdapter.processOAuthLogin(anyString(), anyString()))
+            .willReturn(new GithubUserDto(username, id, avatar, html));
+
+        String requestUri = "/api/login/oauth2/github/callback?code={code}&state={state}";
+        String code = "12345";
+        String state = "abcd";
+        mockMvc.perform(get(requestUri, code, state))
+            .andExpect(status().isOk())
+            .andExpect(header().string("Content-Type", MediaTypes.HAL_JSON_VALUE))
+            .andExpect(jsonPath("$.username").value(username))
+            .andExpect(jsonPath("$.id").value(id))
+            .andExpect(jsonPath("$.avatarUrl").value(avatar))
+            .andExpect(jsonPath("$.htmlUrl").value(html))
+            .andExpect(jsonPath("$._links.self.href").value("http://localhost/api/login/oauth2/github/callback?code=12345&state=abcd"));
+
+        then(githubOauthAdapter)
+            .should(times(1))
+            .processOAuthLogin(code, state);
+
     }
 }
