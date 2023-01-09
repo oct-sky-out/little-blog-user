5d026cb3f529d49e65e746063ac23e27ea4695ea
oct-sky-out <kms3335k@naver.com>
2023-01-09 18:48:00 +0900

    토큰 재발급요청 API 테스트 및 구현 외 1건 1. 토큰 재발급요청 API 테스트 및 구현 2. 토큰 검증 및 해석시 개인정보 암호화 해제 후 토큰정보 제공

--- src/main/java/com/octskyout/users/token/controller/TokenVerifyController.java
+++ src/main/java/com/octskyout/users/token/controller/TokenVerifyController.java
@@ -3,33 +3,110 @@ package com.octskyout.users.token.controller;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
+import com.auth0.jwt.exceptions.TokenExpiredException;
+import com.octskyout.users.oauth.github.dto.GithubUserDto;
+import com.octskyout.users.token.AccessRefreshTokens;
 import com.octskyout.users.token.JWTUtil;
 import com.octskyout.users.token.JwtDecoded;
+import com.octskyout.users.token.TokenType;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.Callable;
+import java.util.stream.Stream;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/token")
 @RequiredArgsConstructor
 public class TokenVerifyController {
     private final JWTUtil jwtUtil;
 
+    @SneakyThrows
+    private static <T> EntityModel<Map<String, T>> wrapTokenExpiredException(
+        Callable<EntityModel<Map<String, T>>> callable, Callable<Void> exceptAfterDo) {
+        try {
+            return callable.call();
+        } catch (TokenExpiredException e) {
+            log.error("TokenVerifyController - 토큰 만료에러");
+            Stream.of(e.getStackTrace())
+                .forEach(stackTraceElement -> log.error(
+                "TokenVerifyController - " + "line " + stackTraceElement.getLineNumber() +
+                    "message : " + stackTraceElement.getModuleName() +
+                    stackTraceElement.getClassName() + stackTraceElement.getMethodName()));
+
+            if (Objects.nonNull(exceptAfterDo)) {
+                exceptAfterDo.call();
+            }
+        }
+
+        return null;
+    }
+
     @GetMapping(value = "/verify", headers = HttpHeaders.AUTHORIZATION)
-    public EntityModel<Map<String,JwtDecoded>> verifyJwtToken(@RequestHeader(HttpHeaders.AUTHORIZATION) String jwtBearerToken) {
+    public EntityModel<Map<String, JwtDecoded>> verifyJwtToken(
+        @Valid @NotEmpty @RequestHeader(HttpHeaders.AUTHORIZATION) String jwtBearerToken,
+        HttpServletResponse response) {
         String jwtToken = getJwtToken(jwtBearerToken);
-        JwtDecoded decodedJWT = jwtUtil.verifyToken(jwtToken);
+        EntityModel<Map<String, JwtDecoded>> selfRel =
+            methodOn(TokenVerifyController.class).verifyJwtToken(jwtBearerToken, response);
+        return wrapTokenExpiredException(() -> {
+            JwtDecoded decodedJWT = jwtUtil.verifyToken(jwtToken);
 
-        EntityModel<Map<String,JwtDecoded>> selfRel =
-            methodOn(TokenVerifyController.class).verifyJwtToken(jwtBearerToken);
+            return EntityModel.of(Map.of("token", decodedJWT), linkTo(selfRel).withSelfRel());
+        }, () -> {
+            response.sendRedirect("/api/token/reissue");
+            return null;
+        });
+    }
 
-        return EntityModel.of(Map.of("token", decodedJWT),
-            linkTo(selfRel).withSelfRel());
+    @GetMapping("/reissue")
+    public EntityModel<Map<String, String>> guideReissueToken() {
+        EntityModel<Map<String, String>> selfRel =
+            methodOn(TokenVerifyController.class).guideReissueToken();
+        EntityModel<Map<String, AccessRefreshTokens>> reissue =
+            methodOn(TokenVerifyController.class).reissueToken(null);
+
+        return EntityModel.of(Map.of("require", "refreshToken"), linkTo(selfRel).withSelfRel(),
+            linkTo(reissue).withRel("reissue"));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(value = "/reissue", headers = HttpHeaders.AUTHORIZATION)
+    public EntityModel<Map<String, AccessRefreshTokens>> reissueToken(
+        @Valid @NotEmpty @RequestHeader(HttpHeaders.AUTHORIZATION) String jwtRefreshBearerToken){
+        String refreshToken = getJwtToken(jwtRefreshBearerToken);
+        EntityModel<Map<String, AccessRefreshTokens>> selfRel =
+            methodOn(TokenVerifyController.class).reissueToken(jwtRefreshBearerToken);
+        return wrapTokenExpiredException(() -> {
+            JwtDecoded decodedRefreshToken = jwtUtil.verifyToken(refreshToken);
+            GithubUserDto githubUserDto =
+                new GithubUserDto(decodedRefreshToken.payload().username(), null, null,
+                    decodedRefreshToken.payload().githubProfile(),
+                    decodedRefreshToken.payload().email());
+
+            String access = jwtUtil.createToken(githubUserDto, TokenType.ACCESS);
+            String refresh = jwtUtil.createToken(githubUserDto, TokenType.REFRESH);
+            return EntityModel.of(Map.of("token", new AccessRefreshTokens(access, refresh)),
+                linkTo(selfRel).withSelfRel());
+        }, () -> {
+            throw new RuntimeException("로그인 세션이 만료되었습니다. 처음부터 로그인해주세요.");
+        });
     }
 
     private String getJwtToken(String jwtBearerToken) {
--- src/test/java/com/octskyout/users/token/controller/TokenVerifyControllerTest.java
+++ src/test/java/com/octskyout/users/token/controller/TokenVerifyControllerTest.java
@@ -1,16 +1,26 @@
 package com.octskyout.users.token.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.octskyout.users.token.JWTUtil;
 import com.octskyout.users.token.JwtDecoded;
+import com.octskyout.users.token.TokenType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import static org.mockito.BDDMockito.then;
+import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -19,6 +29,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 @WebMvcTest(controllers = TokenVerifyController.class)
 class TokenVerifyControllerTest {
@@ -64,15 +75,86 @@ class TokenVerifyControllerTest {
     }
 
     @Test
-    void 토큰이_만료되어_비정상적인_토큰임을_응답한다() {
+    void 액세스_토큰이_만료되어_비정상적인_토큰임을_응답한다() throws Exception {
+        String expiredToken =
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJvY3Qtc2t5LW91dCIsImdpdGh1YlByb2ZpbGUiOiJFc0YwT3ZLaXh1Sk50Wk9uaWxzUUJML1BTMS9rSGpGSmVFTExzbTREak40PSIsImV4cCI6MTY3Mjk5MDIxNywiaWF0IjoxNjcyOTkwMjE3LCJ1c2VybmFtZSI6Ik9VcUNESU1TcERNS0VHSmJWejAzT2c9PSJ9.BzRkecNt1S0NpabOwnu56Z-4l6PGFSA-UrSSA8iLvoM";
 
+        given(jwtUtil.verifyToken(expiredToken))
+            .willThrow(TokenExpiredException.class);
+
+        mockMvc.perform(get("/api/token/verify")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + expiredToken))
+            .andExpect(status().is3xxRedirection())
+            .andExpect(header().exists(HttpHeaders.LOCATION))
+            .andDo(print());
     }
 
     @Test
-    void 토큰_재발급_시도로_요청할_경우_refreshToken이_유효한지_검사한다() {
+    void 토큰_재발급_리다이렉션_요청에_응답한다() throws Exception {
+        mockMvc.perform(get("/api/token/reissue"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$._links.self.href").value("http://localhost/api/token/reissue"))
+            .andExpect(jsonPath("$.require").value("refreshToken"))
+            .andDo(print());
     }
 
     @Test
-    void refreshToken조차_만료되었을_경우_github_재로그인_시도를_수행한다() {
+    void 토큰_재발급_시도로_요청할_경우_refreshToken이_유효한지_검사한다() throws Exception {
+        String refreshToken =
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJvY3Qtc2t5LW91dCIsImdpdGh1YlByb2ZpbGUiOiJFc0YwT3ZLaXh1Sk50Wk9uaWxzUUJML1BTMS9rSGpGSmVFTExzbTREak40PSIsImV4cCI6MzMyMzAwNzk1MjQsImlhdCI6MTY3MzE3MDcyNCwiZW1haWwiOm51bGwsInVzZXJuYW1lIjoiT1VxQ0RJTVNwRE1LRUdKYlZ6MDNPZz09In0.G_7-rkD3_991FrI9GoU3hnIarMCb-Dx_946awdctAF4";
+        String accessToken = "accessToken";
+        String newRefreshToken = "refreshToken";
+        JwtDecoded.Header header = new JwtDecoded.Header("jwt", "HS256");
+        JwtDecoded.Payload payload = new JwtDecoded.Payload(
+            "oct-sky-out",
+            "http://github.com/example",
+            123L,
+            123L,
+            null,
+            "username"
+        );
+
+        given(jwtUtil.verifyToken(refreshToken))
+            .willReturn(new JwtDecoded(header, payload));
+        given(jwtUtil.createToken(any(), eq(TokenType.ACCESS)))
+            .willReturn(accessToken);
+        given(jwtUtil.createToken(any(), eq(TokenType.REFRESH)))
+            .willReturn(newRefreshToken);
+
+        mockMvc.perform(post("/api/token/reissue")
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + refreshToken))
+            .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.token.accessToken").exists())
+            .andExpect(jsonPath("$.token.refreshToken").exists())
+            .andExpect(jsonPath("$._links.self.href").value("http://localhost/api/token/reissue"));
+
+        then(jwtUtil)
+            .should(times(1))
+            .verifyToken(refreshToken);
+        then(jwtUtil)
+            .should(times(1))
+            .createToken(any(), eq(TokenType.ACCESS));
+        then(jwtUtil)
+            .should(times(1))
+            .createToken(any(), eq(TokenType.REFRESH));
+    }
+
+    @Test
+    void refreshToken조차_만료되었을_경우_예외를_발생시킨_후_로그인_href를_전송한다() throws Exception {
+        String expiredRefreshToken =
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJvY3Qtc2t5LW91dCIsImdpdGh1YlByb2ZpbGUiOiJFc0YwT3ZLaXh1Sk50Wk9uaWxzUUJML1BTMS9rSGpGSmVFTExzbTREak40PSIsImV4cCI6MTY3Mjk5MDIxNywiaWF0IjoxNjcyOTkwMjE3LCJ1c2VybmFtZSI6Ik9VcUNESU1TcERNS0VHSmJWejAzT2c9PSJ9.BzRkecNt1S0NpabOwnu56Z-4l6PGFSA-UrSSA8iLvoM";
+
+        given(jwtUtil.verifyToken(expiredRefreshToken))
+            .willThrow(TokenExpiredException.class);
+
+        assertThatThrownBy(() -> mockMvc.perform(post("/api/token/reissue")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + expiredRefreshToken))
+            .andReturn())
+            .hasMessageContaining("로그인 세션이 만료되었습니다. 처음부터 로그인해주세요.");
+
+        then(jwtUtil)
+            .should(times(1))
+            .verifyToken(expiredRefreshToken);
     }
 }
