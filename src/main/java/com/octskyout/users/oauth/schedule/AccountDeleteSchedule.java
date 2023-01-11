387a590f5cf73f7e6d5afcccc8cb269075c15372
oct-sky-out <kms3335k@naver.com>
2023-01-11 17:33:43 +0900

    스케줄링 로직 테스트 외 3건 1. 스케줄링 로직 테스트 2. 스케줄링 cron 테스트 3. 스케줄 기능 완성 4. 비동기 Async 설정 및 스케줄링 비동기 처리 추가

---
+++ src/main/java/com/octskyout/users/oauth/schedule/AccountDeleteSchedule.java
@@ -0,0 +1,37 @@
+package com.octskyout.users.oauth.schedule;
+
+import com.octskyout.users.oauth.entity.OauthUser;
+import com.octskyout.users.oauth.repository.OauthUserRepository;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableAsync
+@RequiredArgsConstructor
+public class AccountDeleteSchedule {
+    private final OauthUserRepository oauthUserRepository;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    @Async
+    public void finalLoggedAfterOneYearAccountDelete() {
+        LocalDateTime oneYearsAgo = LocalDate.now()
+            .minusYears(1L)
+            .atTime(0, 0);
+
+        Long threadNumber = Thread.currentThread().getId();
+        Pageable pageable = PageRequest.of(threadNumber.intValue(),30);
+
+        Page<OauthUser> deleteTargets =
+            oauthUserRepository.findLastLoginAtOneYearsAgoUsers(oneYearsAgo, pageable);
+
+        oauthUserRepository.deleteAll(deleteTargets.getContent());
+    }
+}
