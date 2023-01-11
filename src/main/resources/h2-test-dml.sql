16001b046215fba9ce92acfb5ede38702bcda93b
oct-sky-out <kms3335k@naver.com>
2023-01-11 17:31:32 +0900

    1년전 마지막 로그인한 회원들을 조회하는 쿼리 테스트 외 2건 1. 1년전 마지막 로그인한 회원들을 조회하는 쿼리 테스트 2. 마지막 로그인 회원 쿼리 기능 생성 3. dml 추가 및 application.yml의 ddl, dml classpath 구문 추가

--- src/main/resources/h2-test-dml.sql
+++ src/main/resources/h2-test-dml.sql
@@ -1,2 +1,5 @@
 insert into oauth_user (`email`,`username`,`github_id`)
     values ('admin@naver.com', 'admin', 123);
+
+insert into oauth_user (`email`,`username`,`github_id`, `last_login_date_time`)
+values ('admin@naver.com', 'admin', 123, '2021-01-01 00:00:00');
